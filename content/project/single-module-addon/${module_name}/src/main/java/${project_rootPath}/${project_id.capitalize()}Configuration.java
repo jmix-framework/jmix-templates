7d1d3e0229e01d9ddcd65c6e672b7f06acfc3135
Timur Musin <musintd@haulmont.com>
2021-07-29 10:06:57 +0400

    JST-1600 The error is hilighted in the configuration file of a single-module-addon project in IDEA Ultimate

--- content/project/single-module-addon/${module_name}/src/main/java/${project_rootPath}/${project_id.capitalize()}Configuration.java
+++ content/project/single-module-addon/${module_name}/src/main/java/${project_rootPath}/${project_id.capitalize()}Configuration.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.PropertySource;
 import java.util.Collections;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages = {"io.jmix.core.impl.scanning"})
 @ConfigurationPropertiesScan
 @JmixModule(dependsOn = {EclipselinkConfiguration.class, UiConfiguration.class})
 @PropertySource(name = "${project_rootPackage}", value = "classpath:/${project_rootPath}/module.properties")
